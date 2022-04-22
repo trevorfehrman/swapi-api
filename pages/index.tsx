eb9aec0a5504e7b9ec517b25f3a6681639bfe9d3
Trevor Fehrman <trevorfehrman@gmail.com>
2022-04-22 10:07:19 -0700

    good version

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,68 +1,67 @@
 import * as React from 'react';
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 
-const Home: NextPage = () => {
-  const [data, setData] = React.useState<any>();
+export async function getStaticProps() {
+  const lukeResponse = await fetch('https://swapi.dev/api/people/1');
+  const luke = await lukeResponse.json();
 
-  React.useEffect(() => {
-    async function init() {
-      const response = await fetch('https://swapi.dev/api/people/1');
-      const json = await response.json();
-      setData(json);
-    }
+  const homeworldResponse = await fetch(luke.homeworld);
+  const tatooine = await homeworldResponse.json();
 
-    init();
-  }, []);
+  const residentPromises = tatooine.residents.map((resident: string) => fetch(resident));
+  console.log(residentPromises);
 
+  const residentStreams = await Promise.all(residentPromises);
+  console.log(residentStreams);
+
+  const residentStreamPromises = residentStreams.map(stream => stream.json());
+  const residents = await Promise.all(residentStreamPromises);
+  console.log(residents);
+
+  return {
+    props: {
+      luke,
+      tatooine,
+      residents,
+    }, // will be passed to the page component as props
+  };
+}
+
+interface SWAPIData {
+  luke: {
+    name: string;
+    homeworld: string;
+  };
+
+  tatooine: {
+    name: string;
+    residents: string[];
+  };
+  residents: {
+    name: string;
+  }[];
+}
+
+const Home: NextPage<SWAPIData> = ({ luke, tatooine, residents }) => {
   return (
     <div>
-      Hi my name is: {data?.name}
-      <pre>{JSON.stringify(data)}</pre>
-      <Homeworld homeworldUrl={data?.homeworld} />
+      Hi my name is: {luke.name}
+      <Homeworld tatooine={tatooine} residents={residents} />
     </div>
   );
 };
 
-function Homeworld({ homeworldUrl }: { homeworldUrl: string }) {
-  const [data, setData] = React.useState<any>();
-  React.useEffect(() => {
-    async function init() {
-      const response = await fetch(homeworldUrl);
-      const json = await response.json();
-      setData(json);
-    }
-
-    init();
-  }, [homeworldUrl]);
-
+function Homeworld({
+  tatooine,
+  residents,
+}: {
+  tatooine: { name: string };
+  residents: { name: string }[];
+}) {
   return (
     <div>
-      <div>And I live on: {data?.name}</div>
-      {data?.residents &&
-        data.residents.map((resident: any) => (
-          <Resident residentUrl={resident} key={Math.random()} />
-        ))}
-    </div>
-  );
-}
-
-function Resident({ residentUrl }: { residentUrl: string }) {
-  const [data, setData] = React.useState<any>();
-  React.useEffect(() => {
-    async function init() {
-      const response = await fetch(residentUrl);
-      const json = await response.json();
-      setData(json);
-    }
-
-    init();
-  }, [residentUrl]);
-  return (
-    <div>
-      <h1>{data?.name}</h1>
+      <div>and i live on {tatooine?.name}</div>
+      {residents ? residents.map(resident => <div key={Math.random()}>{resident.name}</div>) : ''}
     </div>
   );
 }
